c7488470f61cd296dcaf6e0c9cb2b98b00a18ee9
qasemB <mahdicmptr@gmail.com>
2021-04-01 00:01:14 +0430

    after my text editor 14000111

--- resources/views/livewire/article/article-content.blade.php
+++ resources/views/livewire/article/article-content.blade.php
@@ -4,7 +4,30 @@
       <div class="image text-center w_30_vw m-auto">
         <img class="max_w_100 " src="{{$article->image}}" alt="{{$article->alt_image}}">
       </div>
-      <div class="p-2 text_container">{!! $article->text !!}</div>
+      @php
+        $textArr = explode('---' ,$article->text);
+        $i=1
+      @endphp
+      <div dir="rtl" class="p-2 text_container">
+          @foreach ($textArr as $t)
+                @if ($i % 2 == 0)
+                    <div id="article_lathin_box" dir="ltr" class="alert-secondary rounded px-2 text-left">
+                        @php $text = nl2br($t); $textArr2 = explode('<br />' ,$text); @endphp
+                        @foreach ($textArr2 as $item)
+                            {{$item}}
+                            <br/>
+                        @endforeach
+                    </div>
+                @else
+                    @php $text = nl2br($t); $textArr2 = explode('<br />' ,$text); @endphp
+                    @foreach ($textArr2 as $item)
+                        {{$item}}
+                        <br/>
+                    @endforeach
+                @endif
+            @php $i++; @endphp
+          @endforeach
+      </div>
     </div>
 </div>
 
